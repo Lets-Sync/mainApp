553534fed4c5b613c27fe78df902e131b160d914
Andrew Song <andrewsong61@gmail.com>
2021-01-20 23:44:47 -0800

    Add initial styling for homescreen view

--- src/styles/AppStyles.js
+++ src/styles/AppStyles.js
@@ -3,8 +3,21 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#24305E',
+  },
+  text: {
+    color: '#ffffff',
+    fontSize: 34,
+  },
+  headerFooterStyle: {
+    width: '100%',
+    height: 45,
+    backgroundColor: '#24305E',
+  },
+  textStyle: {
+    textAlign: 'center',
+    color: '#fff',
+    fontSize: 18,
+    padding: 7,
   },
 });
